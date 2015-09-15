016ed9d32fdc6ad854eca1460e3e6dc9026f8fcc
Saroar <saroar9@gmail.com>
2015-09-15 09:46:13 +0000

    Adding a custom color and font to our application name

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Nomster
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add the font path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    #Precomile additional assets
+    config.assets.precomile += %w( .svg .eot .woff .ttf )
   end
 end
