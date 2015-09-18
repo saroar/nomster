31c881ba8ad621fd4aa81943c5744365f9ef351e
Saroar <saroar9@gmail.com>
2015-09-18 10:11:59 +0000

    Adding an Image upload feature for places in  show page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,6 @@ class Photo < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :picture, PictureUploader
+  validates :caption, :presence => true, :length => { :minimum => 3, :maximum => 24 }
+  validates :picture, :presence => true
 end
